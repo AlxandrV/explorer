a26f59003611db199bc81c44c74c83a038fd410c
AlxandrV <a.vigneaud@gmail.com>
2020-06-16 11:27:24 +0200

    Add - folder/files breadcrumb

---
+++ home/news/news.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+</head>
+<body>
+    <?php
+    include '../../breadcrumb.php';
+    echo '<br/>'
+    ?>
+
+    <h1>News</h1>
+    <a href="../accueil.php">Accueil</a>
+    <a href="">News</a>
+    <a href="./fosse/fosse.php">Fosse</a>
+
+</body>
+</html>
